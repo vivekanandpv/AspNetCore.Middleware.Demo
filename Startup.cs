283d1d6a5ab9235d2d895713ebd71fd980b5cbfa
vivek <vivek@athenaeum.in>
2021-05-02 06:53:30 +0530

    done

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,22 +20,44 @@ namespace AspNetCore.Middleware.Demo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            ILogger<Startup> logger = loggerFactory.CreateLogger<Startup>();
 
-            app.UseRouting();
+            //  Middleware 1: Non-terminal
+            app.Use(async (context, next) => {
+                //  pre-next phase
+                logger.LogInformation("MW 1 : Non-terminal : Use : Pre-next");
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //  call to next middleware
+                await next();
+
+                //  post-next phase
+                logger.LogInformation("MW 1 : Non-terminal : Use : Post-next");
             });
+
+            //  Middleware 2: Non-terminal
+            app.Use(async (context, next) =>
+            {
+                //  pre-next phase
+                logger.LogInformation("MW 2 : Non-terminal : Use : Pre-next");
+
+                //  call to next middleware
+                await next();
+
+                //  post-next phase
+                logger.LogInformation("MW 2 : Non-terminal : Use : Post-next");
+            });
+
+            //  Middleware 3: Terminal
+            app.Run(async context =>
+            {
+                //  Task of the terminal middleware is to prepare the response and thus terminate the chain
+                logger.LogInformation("MW 3 : Terminal : Run");
+                await context.Response.WriteAsync("MW3 writes response for the incoming HTTP request");
+            });
+
+            //  Any middleware from down here, do not run as the chain is terminated
         }
     }
 }
