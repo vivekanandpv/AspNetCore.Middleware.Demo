de124f70412ee259315a728352a78db21cbe758d
vivek <vivek@athenaeum.in>
2021-05-11 19:32:56 +0530

    done

--- Startup.cs
+++ Startup.cs
@@ -24,40 +24,28 @@ namespace AspNetCore.Middleware.Demo
         {
             ILogger<Startup> logger = loggerFactory.CreateLogger<Startup>();
 
-            //  Middleware 1: Non-terminal
+            //  Non-terminal used as terminal
             app.Use(async (context, next) => {
-                //  pre-next phase
-                logger.LogInformation("MW 1 : Non-terminal : Use : Pre-next");
-
-                //  call to next middleware
-                await next();
-
-                //  post-next phase
-                logger.LogInformation("MW 1 : Non-terminal : Use : Post-next");
+                logger.LogInformation("MW1 : As terminal : Use");
+                await context.Response.WriteAsync("MW1 writes response for the incoming HTTP request");
             });
 
-            //  Middleware 2: Non-terminal
+            //  Below chain will be ignored as next() is absent in MW1
+
             app.Use(async (context, next) =>
             {
-                //  pre-next phase
                 logger.LogInformation("MW 2 : Non-terminal : Use : Pre-next");
 
-                //  call to next middleware
                 await next();
 
-                //  post-next phase
                 logger.LogInformation("MW 2 : Non-terminal : Use : Post-next");
             });
 
-            //  Middleware 3: Terminal
             app.Run(async context =>
             {
-                //  Task of the terminal middleware is to prepare the response and thus terminate the chain
-                logger.LogInformation("MW 3 : Terminal : Run");
+                logger.LogInformation("MW3 : Terminal : Run");
                 await context.Response.WriteAsync("MW3 writes response for the incoming HTTP request");
             });
-
-            //  Any middleware from down here, do not run as the chain is terminated
         }
     }
 }
